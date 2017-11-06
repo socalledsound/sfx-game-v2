221cb013556b08026cdbfa3b81b06cbed10904e2
chris kubick <lrs6000@gmail.com>
2017-11-05 22:45:11 -0800

    hmmm

---
+++ untitled.js
@@ -0,0 +1,28 @@
+	// {
+	// 	title: "CANVAS SUITCASE ZIPPER, LONG ZIP OPEN OR CLOSE, SIDE COMPARTMENT, GOOD, SEVERAL TAKES",
+	// 	fullSound: "sounds/zippers/canvas-suitcase-zipper/cszipper-full.mp3",
+	// 	solutionKey:"A",
+	// 	alreadySolved: false,
+	// 	clips: [
+	// 		{A1:"sounds/zippers/canvas-suitcase-zipper/cszipper1.mp3"},
+	// 		{A2:"sounds/zippers/canvas-suitcase-zipper/cszipper2.mp3"},
+	// 		{A3:"sounds/zippers/canvas-suitcase-zipper/cszipper3.mp3"},
+	// 		{A4:"sounds/zippers/canvas-suitcase-zipper/cszipper4.mp3"},
+	// 		{A5:"sounds/zippers/canvas-suitcase-zipper/cszipper5.mp3"}
+	// 	]
+	// },
+	// 	{
+	// 	title: "A LARGE GROUP OF CHILDREN LAUGHING IN A PRETEND COURTROOM",
+	// 	fullSound: "sounds/monkeys/monkeys-full.mp3",
+	// 	solutionKey:"A",
+	// 	alreadySolved: false,
+	// 	clips: [
+	// 		{A1:"sounds/monkeys/monkeys2.mp3"},
+	// 		{A2:"sounds/monkeys/monkeys1.mp3"},
+	// 		{A3:"sounds/monkeys/monkeys3.mp3"},
+	// 		{A4:"sounds/monkeys/monkeys4.mp3"},
+	// 		{A5:"sounds/monkeys/monkeys5.mp3"}
+	// 	]
+	// },
+
+//cellHiddenColors:[[6, 62, 229],[196, 17, 44],[229, 99, 6],[200,20,200],[11, 221, 25]],
